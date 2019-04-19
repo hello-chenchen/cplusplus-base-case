2edeeef7cca0bdffe3e59efb1c50c275ae5af110
hello_chenchen <welcomechenchen@gmail.com>
2019-04-19 16:22:51 +0800

    常量变量和常量指针的地址不可以赋值给普通变量

--- const.cc
+++ const.cc
@@ -7,16 +7,28 @@ int constValue() {
     const int *pt = &age;
 
     cout << "*pt: " << *pt << endl;
-    // *pt += 1; invalid
+    // *pt += 1; NOTICE: invalid
 
     age = 13;
 
     cout << "*pt1: " << *pt << endl;
 }
 
+int constValue2ConstPoint() {
+    const int age = 26;
+    const int *pt = &age;
+
+    cout << "*pt: " << *pt << endl;
+
+    //NOTICE: invalid
+    // int *age1 = pt;
+    // int *age1 = &age;
+    // cout << "age1: " << age1 << endl;
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    constValue();
+    constValue2ConstPoint();
     return 0;
 }
