2983a721cdedaa94576d237d21b0a636b9010852
hello_chenchen <welcomechenchen@gmail.com>
2019-04-19 15:52:44 +0800

    常量指针不可以通过修改常量指针来改变内存中的值,但是可以通过改变被指向的内存对象的值来改变数据

--- const.cc
+++ const.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int constValue() {
     int age = 26;
     const int *pt = &age;
 
+    cout << "*pt: " << *pt << endl;
     // *pt += 1; invalid
+
+    age = 13;
+
+    cout << "*pt1: " << *pt << endl;
 }
 
 int main(int argc, char const *argv[])
